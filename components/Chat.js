93b559a88944d62429f4352fd434decfa378a35a
Edison Abdiel <edison.abdiel@soundcloud.com>
2021-08-07 19:00:55 +0200

    feat: Connect to Firebase/Firestore. Add renderInputToolBar. Update componentWillMount to Auth the user.

--- components/Chat.js
+++ components/Chat.js
@@ -2,8 +2,31 @@
 import React from 'react';
 // React-Native Components
 import { View, Text, StyleSheet, Platform, KeyboardAvoidingView } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import NetInfo from '@react-native-community/netinfo';
+
 // GiftedChat Components
-import { GiftedChat, ChatMessage, ChatInput, ChatTypingIndicator, Bubble } from 'react-native-gifted-chat';
+import { GiftedChat, InputToolbar, Bubble } from 'react-native-gifted-chat';
+
+
+// Firebase boilerplate
+import firebase from 'firebase';
+import 'firebase/firestore';
+// Config keys
+const firebaseConfig = {
+  apiKey: "AIzaSyAD0lh_wdmptkLJZP9TOCj5AKSBq_VvWpY",
+  authDomain: "react-native-gifted-chat.firebaseapp.com",
+  databaseURL: "shatapp-mobile.appspot.com",
+  projectId: "shatapp-mobile",
+  storageBucket: "shatapp-mobile.appspot.com",
+  messagingSenderId: "672068650030",
+  appId: '1:672068650030:ios:b8c0ccac353a32b24d0bbf',
+}
+
+// Initialize Firebase
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
 
 
 export default class Chat extends React.Component {
@@ -14,33 +37,55 @@ export default class Chat extends React.Component {
     super();
     this.state = {
       messages: [],
-      typing: false
+      uid: 0,
+      user: {
+        _id: '',
+        name: '',
+        avatar: '',
+      },
+      isConnected: false,
     }
   }
+  // Listen for updates in Firestore messages collection.
+  referenceChatMessages = firebase.firestore().collection('messages');
 
   // Sets the state with a static message
   componentDidMount() {
-    this.setState({
-      messages: [
-        {
-          _id: 1,
-          text: 'Hey there',
-          createdAt: new Date(),
-          user: {
-            _id: 2,
-            name: 'John Doe',
-            avatar: 'https://placeimg.com/140/140/any'
-          },
-        },
-        // System message informing about an user joining the chat room
-        {
-          _id: 2,
-          text: `${this.props.route.params.name} entered the chat`,
-          createdAt: new Date(),
-          system: true,
-         },
-      ],
+    const { name } = this.props.route.params;
+    this.props.navigation.setOptions({ title: `${name}` });
+    //Find out connection status
+    NetInfo.fetch().then(connection => {
+      if (connection.isConnected) {
+        this.setState({ isConnected: true });
+        // Authenticates user via Firebase
+        this.authUnsubscribe = firebase.auth()
+          .onAuthStateChanged(async (user) => {
+            if (!user) {
+              await firebase.auth().signInAnonymously();
+            }
+            this.setState({
+              uid: user.uid,
+              user: {
+                _id: user.uid,
+                name: name,
+                avatar: 'https://placeimg.com/140/140/any',
+              },
+              messages: [],
+            });
+            this.unsubscribe = this.referenceChatMessages
+              .orderBy("createdAt", "desc")
+              .onSnapshot(this.onCollectionUpdate);
+          });
+      } else {
+        this.setState({ isConnected: false });
+        this.getMessages();
+      }
     });
+  }
+
+  componentWillUnmount() {
+    this.unsubscribe();
+    this.authUnsubscribe();
   }
 
   // Send a message
@@ -66,21 +111,75 @@ export default class Chat extends React.Component {
     )
   }
 
+  // Reads ann renders the messages from Firebase
+  OnCollectionUpdate = (snapshot) => {
+    const messages = [];
+    snapshot.forEach(doc => {
+      let data = doc.data();
+      messages.push({
+        _id: data._id,
+        text: data.text,
+        createdAt: data.createdAt.toDate(),
+        user: {
+          _id: data.user._id,
+          name: data.user.name,
+          avatar: data.user.avatar,
+        }
+      });
+      this.setState({
+        messages: messages,
+      })
+    });
+  }
+  // Adds a new message
+  addMessages() {
+    const message = this.state.messages[0];
+    this.referenceChatMessages.add({
+      _id: message._id,
+      text: message.text,
+      createdAt: message.createdAt,
+      user: message.user,
+    });
+  }
+
+  // Update  messagess
+  async getMessages() {
+    let messages = '';
+    try {
+      messages = (await AsyncStorage.getItem('messages')) || [];
+      this.setState({
+        messages: JSON.parse(messages)
+      });
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+   //Render the default InputTollbar when the user is online
+   renderInputToolbar = props => {
+    if (this.state.isConnected === false) {
+    } else {
+      return (
+        <InputToolbar {...props} />
+      )
+    }
+  }
+
   render() {
     // Brings params over from home screen name and background color selected
     let { name, backgroundColor } = this.props.route.params;
-    
+
 
     return (
       <View style={[styles.bgcolor(backgroundColor), styles.container]}>
         <View style={styles.chat}>
           <GiftedChat
             renderBubble={this.renderBubble.bind(this)}
+            renderInputToolbar={this.renderInputToolbar}
+            renderUsernameOnMessage={true}
             messages={this.state.messages}
             onSend={messages => this.onSend(messages)}
-            user={{
-              _id: 1,
-            }}
+            user={this.state.user}
           />
           {Platform.OS === 'android' ? <KeyboardAvoidingView
             behavior="height" /> : null}
---
+++ components/dataStructure.js
@@ -0,0 +1,10 @@
+const data = [
+    {
+      text: "Hello developer",
+      createdAt: new Date(),
+      user: {
+        name: "React Native",
+        avatar: "https://placeimg.com/140/140/any",
+      },
+    },
+  ]
